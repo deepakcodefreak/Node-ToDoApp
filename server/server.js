18ec65d60e15fee3b993792559e7232be862b16d
deepakcodefreak <deepak19.engg@gmail.com>
2018-07-18 23:56:56 +0530

    test POST /todos route

---
+++ server/server.js
@@ -0,0 +1,42 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+
+var {mongoose} = require('./db/mongoose.js')
+var {ToDo} = require('./models/todo.js')
+var {Users} = require('./models/Users.js')
+
+// var app = express();
+//
+// app.use(bodyParser.json());
+//
+// app.post('/todos',(req,res)=>{
+//   var newtodo = ToDo({
+//     text:req.body.text
+//   })
+//   newtodo.save().then((doc)=>{
+//     res.send(doc)
+//   },(e)=>{
+//     res.status(400).send(e);
+//   })
+// })
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos',(req,res)=>{
+  var newtodo = new ToDo({
+    text:req.body.text
+  })
+  newtodo.save().then((docs)=>{
+    res.send(docs)
+  },(e)=>{
+    res.status(400).send(e)
+  })
+})
+
+app.listen(3000,()=>{
+  console.log(`started on port 3000`);
+})
+
+module.exports = {app}
---
+++ server/tests/server.test.js
@@ -0,0 +1,54 @@
+const expect = require('expect')
+const request = require('supertest')
+
+const {app} = require('./../server.js');
+const {ToDo} = require('./../models/todo.js')
+
+beforeEach((done)=>{
+  ToDo.remove({}).then(()=>done())
+})
+
+describe('POST /todos',()=>{
+  it('Should create a new todo', (done) => {
+    var text = 'I am testing todo text';
+    request(app)
+    .post('/todos')
+    .send({text})   //json data {text} is automatically converted to string
+    .expect(200)
+    .expect((res)=>{
+      expect(res.body.text).toBe(text);
+    })
+    .end((err,res)=>{
+      if (err) {
+        return done(err);
+      }
+      ToDo.find().then((todos)=>{
+        expect(todos.length).toBe(1);
+        expect(todos[0].text).toBe(text);
+        done();
+      }).catch((err)=>done(err));
+    })
+
+  });
+
+it('Should not add a note', (done) => {
+  request(app)
+  .post('/todos')
+  .send()
+  .expect(400)
+  .end((err,res)=>{
+    if (err) {
+      return done(err);
+    }
+    ToDo.find().then((todos)=>{
+      expect(todos.length).toBe(0)
+      done();
+    }).catch((err)=>done(err))
+
+
+  })
+});
+
+
+
+})
